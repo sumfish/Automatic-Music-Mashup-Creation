badd7c8f2905ab40ea8e5048e70367383c92345c
fishope <831upsummer@gmail.com>
2019-07-16 15:17:41 +0800

    add split audio by beat

--- test_beat.py
+++ test_beat.py
@@ -2,14 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
-from scipy.spatial import distance
-from scipy import ndimage
-from math import sqrt
 import madmom.features.beats as bt
 import madmom.features.downbeats as dbt
+from pydub import AudioSegment #cut
 
-sample = 'musicset/000221.wav'
-sample2 = 'musicset/000226.wav'
+sample = 'musicset/000224.wav'
+
 y, sr = librosa.load(sample)
 #y_harmonic, y_percussive = librosa.effects.hpss(y)
 tempo, beats = librosa.beat.beat_track(y=y,sr=sr)
@@ -27,9 +25,10 @@ plt.vlines(times[beats], 0, 1, alpha=0.5, color='r',
 print(times[beats])
 
 #### madmom
+#proc = BeatTrackingProcessor(fps=100)
 #proc = bt.DBNBeatTrackingProcessor(fps=100)
 #act = bt.RNNBeatProcessor()(sample2)
-proc = dbt.DBNDownBeatTrackingProcessor(beats_per_bar=4, fps=100)
+proc = dbt.DBNDownBeatTrackingProcessor(beats_per_bar=[4, 4], fps=120)
 act = dbt.RNNDownBeatProcessor()(sample)
 plt.vlines(proc(act), 0, 1, alpha=0.5, color='g',
             linestyle='--', label='Beats_2')
@@ -42,4 +41,28 @@ plt.gca().xaxis.set_major_formatter(librosa.display.TimeFormatter())
 plt.tight_layout()
 plt.savefig("beat_vs_onset.png")
 plt.show()
+
+# split audio
+sound = AudioSegment.from_file(sample)
+# cut by miliseconds
+opening=0
+end=0
+has_record=False
+count=0
+bar4=0
+for s, index in proc(act):
+    if(index==1):
+        if has_record :
+            end=s
+            bar4=bar4+1
+            if(bar4%4==0):
+                cut=sound[opening*1000:end*1000]
+                cut.export("cut"+str(count)+".wav",format='wav')
+                opening=s
+                count=count+1
+                bar4=0
+        else:    
+            opening=s
+            has_record=True
+
 

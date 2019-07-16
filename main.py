b9502895eb6bbf7f7cc7046e4667c627abef5281
fishope <831upsummer@gmail.com>
2019-07-16 17:42:30 +0800

    add generation:time-stretch

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyrubberband as pyrb
 import librosa
 import os
 import mashability as mas
+from pydub import AudioSegment #mix
 
 input_phrase='000229.wav'
 input_path='./musicset/'
@@ -15,6 +16,7 @@ def main():
     input_chroma, input_spect, input_tempo = mas.chroma_and_spectral(input_path+input_phrase)
     stable_rate = mas.harmonic_complex(input_chroma)
 
+    '''
     #dataset candidate
     print("----candidate-----")
     V_mashability=0
@@ -22,7 +24,7 @@ def main():
     chosed_wave=''
     for candidate in os.listdir(can_path):
         if(candidate.endswith('.wav') and not candidate.endswith(input_phrase)):
-            print(candidate+' :')
+            print('------'+candidate+' :')
             S_v,best_pitch=mas.mashibility(input_chroma, input_spect, input_tempo, stable_rate, can_path+candidate)
             
             # choose the best-match
@@ -31,23 +33,24 @@ def main():
                 pitch_shift=best_pitch
                 chosed_wave=candidate   
     print(V_mashability, pitch_shift, chosed_wave)
-
+    '''
     #generation
-    generation(chosed_wave, pitch_shift, input_chroma)
+    generation("000225.wav", 10, input_chroma, input_tempo)
+    #generation(chosed_wave, pitch_shift, input_chroma, input_tempo)
     
-def generation(matched_wave, pitch, input_chroma):
+def generation(matched_wave, pitch, input_chroma, input_tempo):
     print("---Generating.....---")
     print("choose:{}".format(matched_wave))
     y, sr = librosa.load(can_path+matched_wave)
     
-    # pitch shifting
+    # pitch shifting (maybe a little difference after shifting)
     y_shift = pyrb.pitch_shift(y, sr, n_steps=-pitch)
     #y_shift = librosa.effects.pitch_shift(y, sr, n_steps=pitch) #by liborsa
+    y_harmonic, y_percussive = librosa.effects.hpss(y_shift)
+    y_tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,sr=sr)
 
     '''
     # checked by hormonic
-    y_harmonic, y_percussive = librosa.effects.hpss(y_shift)
-    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,sr=sr)
     chromagram = librosa.feature.chroma_cqt(y=y_harmonic,sr=sr)
     # We'll use the median value of each feature between beat frames                                   
     beat_chroma = librosa.util.sync(chromagram,
@@ -57,9 +60,15 @@ def generation(matched_wave, pitch, input_chroma):
     can_chroma24 =np.concatenate((beat_chroma,beat_chroma),axis=0)
     mas.harmonic(input_chroma,can_chroma24)
     '''
-    
+
     # time stretch
-    #librosa.effects.time_stretch
+    rate =float(input_tempo)/y_tempo
+    print("stretch_rate:{}".format(rate))
+    #librosa.effects.time_stretch(y_shift, rate)
+    y_stretch_shift=pyrb.time_stretch(y_shift, sr, rate)
+    librosa.output.write_wav('candidate.wav',y_stretch_shift, sr)
+
+    
 
 
 

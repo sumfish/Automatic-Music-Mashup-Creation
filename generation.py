f54b084f7b0ddfab47ebd3fc2428be85b29cca11
fishope <831upsummer@gmail.com>
2019-07-24 10:40:21 +0800

    some modification

--- generation.py
+++ generation.py
@@ -8,29 +8,32 @@ import pyrubberband as pyrb
 from pydub import AudioSegment #mix
 import librosa
 
-input_phrase='000229.wav'
-input_path='../../musicset/'
-can_path='./smallset/'
+input_path='../../hip-pop/'
+can_path='./hip-pop/'
 can_output='ps_ts.wav'
 output_path='./output_audio/'
 
 
 def generation(matched_wave, pitch, input_chroma, input_tempo, input_phrase):
     print("choose:{}".format(matched_wave))
-    y_shift=pitchShift(can_path+matched_wave, pitch)
+    ## change dir to output_path in pitchShift()
+    y_shift=pitchShift(matched_wave, pitch, input_phrase)
 
     print("---Generating time stretching file---")
-    os.chdir(output_path+input_phrase[:-4])
     timeStretch(y_shift, input_tempo)
 
     print("---Generating volume adjust file(nor)---")
-    volumeNor()
+    volumeNor(input_phrase)
 
     print("---Generating mixed file---")
-    mixed()
+    mixed(input_phrase)
 
-def pitchShift(loop, pitch):
-    y, sr = librosa.load(loop,sr=44100)
+def pitchShift(loop, pitch, input_phrase):
+    y, sr = librosa.load(can_path+loop,sr=44100)
+
+    ## change dir
+    os.chdir(output_path+input_phrase[:-4])
+    sf.write(loop, y, samplerate=44100) #original candidate
     # pitch shifting (maybe a little difference after shifting)
     y_shift = pyrb.pitch_shift(y, sr, n_steps=-pitch)
     #y_shift = librosa.effects.pitch_shift(y, sr, n_steps=pitch) #by liborsa
@@ -57,26 +60,24 @@ def timeStretch(y_shift, input_tempo):
     print("stretch_rate:{}".format(rate))
     #librosa.effects.time_stretch(y_shift, rate) #by liborsa
     y_stretch_shift=pyrb.time_stretch(y_shift, sr, rate)
-    # seg11
     sf.write(can_output, y_stretch_shift, samplerate=44100)    
     #librosa.output.write_wav('candidate.wav',y_stretch_shift, sr=44100) #bit will be 64
 
-def volumeNor():
+def volumeNor(input_phrase):
     FFMPEG_CMD = "ffmpeg-normalize"
-    cmd2=FFMPEG_CMD+' -v -f '+input_path+input_phrase+' -o '+input_phrase[:-4]+'_normalized.wav'
+    cmd2=FFMPEG_CMD+' -v -f '+input_path+input_phrase+' -o '+input_phrase[:-4]+'_input_vn.wav'
     subprocess.Popen(shlex.split(cmd2))
-    cmd=FFMPEG_CMD+' -v -f '+can_output+' -o '+can_output[:-4]+'_normalized.wav'
+    cmd=FFMPEG_CMD+' -v -f '+can_output+' -o '+can_output[:-4]+'_vn.wav'
     subprocess.Popen(shlex.split(cmd))
     
 
-def mixed():
+def mixed(input_phrase):
     # wait for cmd ffmpeg
     while True:   
-        if os.path.isfile(can_output[:-4]+'_normalized.wav') and os.path.isfile(input_phrase[:-4]+'_normalized.wav'):
-            can_wave=AudioSegment.from_file(can_output[:-4]+'_normalized.wav')
-            input_wave=AudioSegment.from_file(input_phrase[:-4]+'_normalized.wav')
+        if os.path.isfile(can_output[:-4]+'_vn.wav') and os.path.isfile(input_phrase[:-4]+'_input_vn.wav'):
+            can_wave=AudioSegment.from_file(can_output[:-4]+'_vn.wav')
+            input_wave=AudioSegment.from_file(input_phrase[:-4]+'_input_vn.wav')
             combined=input_wave.overlay(can_wave) #if can is longer than input, will be cut
-            # seg11
             combined.export('combination.wav',format='wav')
             break
         else:
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import mashability as mas
-import generation  as gen
+import generation  as gene
 import os
 
-input_phrase='000229.wav'
-input_path='./musicset/'
-can_path='./smallset/'
+input_phrase='000295.wav'
+#input_path='./musicset/'
+input_path='./hip-pop/'
+can_path='./hip-pop/'
 output_path='./output_audio/'
 
 def main():
@@ -15,13 +16,13 @@ def main():
             os.makedirs(output_path+input_phrase[:-4])
         except FileExistsError:
             print('have existed dir')
-                
+    
     print("----input-----")
     input_chroma, input_spect, input_tempo = mas.chroma_and_spectral(input_path+input_phrase)
+    print("input chroma shape:{}".format(input_chroma.shape[1]))
     print("input tempo:{}".format(input_tempo))
     stable_rate = mas.harmonic_complex(input_chroma)
 
-    '''
     #dataset candidate
     print("----search candidate-----")
     V_mashability=0
@@ -37,12 +38,13 @@ def main():
                 V_mashability=S_v
                 pitch_shift=best_pitch
                 chosed_wave=candidate   
+                #candidate size
     print(V_mashability, pitch_shift, chosed_wave)
-    '''
+    
     # generation
     print("---Generating---")
-    gen.generation("000225.wav", 10, input_chroma, input_tempo, input_phrase)
-    #generation(chosed_wave, pitch_shift, input_chroma, input_tempo)
+    #gene.generation("000225.wav", 10, input_chroma, input_tempo, input_phrase)
+    gene.generation(chosed_wave, pitch_shift, input_chroma, input_tempo, input_phrase)
     
 
 if __name__ == "__main__":
